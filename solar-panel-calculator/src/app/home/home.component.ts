2d7a8acada88291a7f178516cf6d1431bec465d2
Riley Spering <speringriley@gmail.com>
2024-02-14 16:38:23 -0600

    added nav bar and footer

--- solar-panel-calculator/src/app/home/home.component.ts
+++ solar-panel-calculator/src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NavBarComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- solar-panel-calculator/src/app/results/results.component.ts
+++ solar-panel-calculator/src/app/results/results.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent, FooterComponent],
   templateUrl: './results.component.html',
   styleUrl: './results.component.css'
 })
