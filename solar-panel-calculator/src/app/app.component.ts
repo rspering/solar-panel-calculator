0819a077d9ddbb95fc3645b753bd9eb58c20b109
Riley Spering <speringriley@gmail.com>
2024-02-14 13:38:51 -0600

    added routing and basic layouts

--- solar-panel-calculator/src/app/app.component.ts
+++ solar-panel-calculator/src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResultsComponent } from './results/results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomeComponent, ResultsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- solar-panel-calculator/src/app/app.routes.ts
+++ solar-panel-calculator/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResultsComponent } from './results/results.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {'path': '', component: HomeComponent},
+  {'path': 'results', component: ResultsComponent}
+];
