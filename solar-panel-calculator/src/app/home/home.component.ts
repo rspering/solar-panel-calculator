ae6277657a386bf4501797eea40b3963be397319
Reid Haegele <haegelereid@gmail.com>
2024-02-24 18:18:26 -0600

    Google Maps API integration

--- solar-panel-calculator/src/app/home/home.component.ts
+++ solar-panel-calculator/src/app/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { Inject } from '@angular/core';
+import { FormsModule, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, NavBarComponent, FooterComponent],
+  imports: [RouterLink, NavBarComponent, FooterComponent, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  checkoutForm = this.formBuilder.group({
+    address: ''
+  });
+  inputData!: string;
 
+  constructor(@Inject(Router) private router: Router, private formBuilder: FormBuilder) {}
+
+  onSubmit(): void {
+    // Navigate to another page along with the input data
+    this.inputData = this.checkoutForm.value.address!;
+    this.router.navigate(['/results', this.inputData]);
+  }
 }
--- solar-panel-calculator/src/app/results/results.component.ts
+++ solar-panel-calculator/src/app/results/results.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { DataDisplayFieldComponent } from '../data-display-field/data-display-field.component';
+import { GoogleMapsModule } from '@angular/google-maps'
+import { Inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [NavBarComponent, FooterComponent, DataDisplayFieldComponent],
+  imports: [NavBarComponent, FooterComponent, DataDisplayFieldComponent, GoogleMapsModule],
   templateUrl: './results.component.html',
   styleUrl: './results.component.css'
 })
-export class ResultsComponent {
+export class ResultsComponent implements OnInit {
+  receivedData!: string;
+  cen!: google.maps.LatLng;
+  mapOptions: google.maps.MapOptions = {
+    center: undefined,
+    zoom: 18,
+  };
 
+  constructor(@Inject(ActivatedRoute) private route: ActivatedRoute) {}
+  
+  ngOnInit() {
+    // Retrieve the data passed from the input form component
+    this.route.params.subscribe(params => {
+      this.receivedData = params['data'];
+    });
+
+    const geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { 'address': this.receivedData}, (results, status) => {
+      if (status == 'OK') {
+        this.cen = results![0].geometry.location;
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
 }
